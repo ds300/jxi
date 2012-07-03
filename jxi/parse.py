623e40b2f3aae9266c74543998abae52a3e19bfd
ds300 <ds300@sussex.ac.uk>
2012-07-04 00:07:04 +0100

    cleaned up and commented lexer code

--- jxi/parse.py
+++ jxi/parse.py
@@ -1,7 +1,7 @@
-import re, sys, pyjxi, string
+import string
 """
 Author: David Sheldrick
-Date: 2012-07-02
+Date: 2012-07-03
 """
 
 ###################
@@ -29,27 +29,19 @@ def lex(input_text):
 	This is obviously the jxi lexical analyser. It is of straightforward design
 	"""
 
-	# The regular expressions we'll be using
 	whitespace = set([",", " ", "\v", "\t", "\n", "\r", "\f"])
-	# this next regex with thanks to Doug Crockford or whoever drew 
-	# that diagram on the json website
+	symbols = set(['<','>','[',']','{','}',':','/', '='])
+
 	digits = set([str(i) for i in range(10)])
 	digits_sans_zero = set([str(i) for i in range(1,10)])
 	number_start_chars = digits | set(["-"])
-	print "nmstrc", number_start_chars
-	re_num = re.compile(r'^-?(0|[1-9]\d*)(\.\d+)?((e|E)(\+|-)?\d+)?')
-
-	# the various symbols we need to recognise
-	symbols = set(['<','>','[',']','{','}',':','/', '='])
+	
 	letters = set([c for c in string.lowercase + string.uppercase])
 	word_chars = letters | set(["_"]) | digits
+
 	json_string_escapes = {}
 	for delim in ['"', "'"]:
 		json_string_escapes[delim] = set(["\b", "\f", "\n", "\r", "\t", "\\", delim])
-
-	raw_string_escapes = {}
-	for delim in ["`"]:
-		raw_string_escapes[delim] = set(["\\", delim])
 
 	control_characters = {
 		"b": "\b",
@@ -60,13 +52,21 @@ def lex(input_text):
 		"\\": "\\",
 		"/": "/"
 	}
+
+	for delim in json_string_escapes:
+		control_characters[delim] = delim
+
+	raw_string_escapes = {}
+	for delim in ["`"]:
+		raw_string_escapes[delim] = set(["\\", delim])
+
 	reserved_word_types = {"null":"null", "true":"bool", "false":"bool"}
 
 	line = 1
 	line_start_char = 0
 
 	inp = input_text
-	i=0
+	i = 0
 
 	size = len(input_text)
 
@@ -76,7 +76,7 @@ def lex(input_text):
 		while inp[i] in whitespace:
 			if inp[i] == "\n":
 				line += 1
-				line_start_char = 0
+				line_start_char = i
 			i += 1
 
 		# figure out what type of token we're dealing with
@@ -96,30 +96,39 @@ def lex(input_text):
 
 		### JSON STRINGS ###
 		elif inp[i] in json_string_escapes:
+			# get the delimiter being used and the corresponding escape chars
 			delim = inp[i]
 			escapes = json_string_escapes[delim]
+
+			# skip over delimiter
 			i += 1
 			text = ""
+
+			# keep going until we see the delimiter again
 			while inp[i] != delim:
+				# get the next sequence of chars not containing escapes and concat
 				j = i
 				while inp[j] not in escapes:
 					j += 1
 				text += inp[i:j]
 				i = j
+
+				# now we're at a character that requires action to be taken
 				if inp[i] == "\\":
 					# we need to escape something
 					i += 1
-					if inp[i] == delim:
-						text += delim
-						i += 1
 
-					elif inp[i] in control_characters:
+					### CONTROL CHARS ###
+					if inp[i] in control_characters:
+						# put the control character in the string
 						text += control_characters[inp[i]]
 						i += 1
 
+					### UNICODE STUFFS ###
 					elif inp[i] == "u":
-						# do the unicode thing
-						# this algorithm also with thanks to Doug Crockford
+						# do the 4-hexit unicode thing
+						# this algorithm shamelessly stolen from Doug Crockford's 
+						# javascript json parser (the one that doesn't use eval)
 						charcode = 0
 						i += 1
 						for j in range(4):
@@ -137,10 +146,10 @@ def lex(input_text):
 						raise JXIParseError(msg, line, line_start_char, i)
 
 				elif inp[i] != delim:
-					# ignore for now
-					# msg = "Unescaped %s detected in string literal" % repr(inp[i])
-					# raise JXIParseError(msg, line, line_start_char, i)
-					i += 1
+					msg = "Unescaped %s detected in string literal" % repr(inp[i])
+					raise JXIParseError(msg, line, line_start_char, i)
+
+			# skip over final delimiter
 			i += 1
 			yield ("string", text)
 
@@ -196,21 +205,32 @@ def lex(input_text):
 
 		### RAW STRINGS ###
 		elif inp[i] in raw_string_escapes:
+			# get delimiter and related escapes
 			delim = inp[i]
 			escapes = raw_string_escapes[delim]
+
+			# skip over delimiter
 			i += 1
 			text = ""
-			while inp[i] != delim and i < size:
+
+			# keep going until we see the delimiter again
+			while inp[i] != delim:
+				# get the next string of uninteresting chars and concat
 				j = i
-				while inp[j] not in escapes and j < size:
+				while inp[j] not in escapes:
 					j += 1
 				text += inp[i:j]
 				i = j
+
+				# now we're at an interesting char
 				if inp[i] == "\\":
 					i += 1
+					# only need to escape if it's the delimiter
 					if inp[i] == delim:
 						text += delim
 					else:
+						# otherwise, that backslash was meant to be there
+						# better put it back in
 						text += "\\"
 
 			# ignore final delimiter
@@ -220,7 +240,6 @@ def lex(input_text):
 		else:
 			msg = "Illegal character %s" % repr(inp[i])
 			raise JXIParseError(msg, line, line_start_char, i)
-
 
 	yield ("EOF", "EOF")
 
